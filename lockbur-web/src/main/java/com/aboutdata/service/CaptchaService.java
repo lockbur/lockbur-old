8eed3cea92f4e35c6c1e9cfdebd4f9399502c021
wangkun <845885222@qq.com>
2016-10-16 14:49:55 +0800

    ADD#验证码

--- lockbur-web/src/main/java/com/aboutdata/service/CaptchaService.java
+++ lockbur-web/src/main/java/com/aboutdata/service/CaptchaService.java
@@ -1,6 +1,9 @@
 package com.aboutdata.service;
 
 
+import org.apache.shiro.web.session.HttpServletSession;
+
+import javax.servlet.http.HttpServletRequest;
 import java.awt.image.BufferedImage;
 /**
  * Service - 验证码
@@ -13,18 +16,18 @@ public interface CaptchaService {
     /**
      * 生成验证码图片
      *
-     * @param captchaId 验证ID
+     * @param session 验证ID
      * @return 验证码图片
      */
-    BufferedImage buildImage(String captchaId);
+    BufferedImage buildImage(HttpServletSession session);
 
     /**
      * 验证码验证
      *
-     * @param captchaId 验证ID
+     * @param session 验证ID
      * @param captcha 验证码(忽略大小写)
      * @return 验证码验证是否通过
      */
-    boolean isValid(String captchaId, String captcha);
+    boolean isValid(HttpServletSession session, String captcha);
 
 }
--- lockbur-web/src/main/java/com/aboutdata/service/impl/CaptchaServiceImpl.java
+++ lockbur-web/src/main/java/com/aboutdata/service/impl/CaptchaServiceImpl.java
@@ -2,35 +2,44 @@ package com.aboutdata.service.impl;
 
 
 import com.aboutdata.service.CaptchaService;
+
+import java.awt.Color;
+
+import com.github.bingoohuang.patchca.Patchca;
+import com.github.bingoohuang.patchca.color.SingleColorFactory;
+import com.github.bingoohuang.patchca.custom.ConfigurableCaptchaService;
+import com.github.bingoohuang.patchca.filter.predefined.CurvesRippleFilterFactory;
+import com.github.bingoohuang.patchca.service.Captcha;
+import com.github.bingoohuang.patchca.utils.encoder.EncoderHelper;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.web.session.HttpServletSession;
 import org.springframework.stereotype.Service;
+
 import java.awt.image.BufferedImage;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Service - 验证码
- *
- *
- *
  */
 @Service("captchaServiceImpl")
 public class CaptchaServiceImpl implements CaptchaService {
 
-    @Override
-    public BufferedImage buildImage(String captchaId) {
+    private static String captchaId = "Patchca";
 
-        return null;
+    @Override
+    public BufferedImage buildImage(HttpServletSession session) {
+        Captcha captcha = Patchca.next();
+        session.setAttribute(captchaId, captcha.getChallenge());
+        return captcha.getImage();
     }
 
     @Override
-    public boolean isValid(String captchaId, String captcha) {
-        if (StringUtils.isNotEmpty(captchaId) && StringUtils.isNotEmpty(captcha)) {
-            try {
-//                return imageCaptchaService.validateResponseForID(captchaId, captcha.toUpperCase());
-                return true;
-            } catch (Exception e) {
-                return false;
-            }
+    public boolean isValid(HttpServletSession session, String captcha) {
+        String captchaWord = session.getAttribute(captchaId).toString();
+        if (StringUtils.isNotEmpty(captcha)) {
+            return captcha.equalsIgnoreCase(captchaWord);
         } else {
             return false;
         }
